9583ded8c9f0f6dc7c37e077687396f281748f44
João Paulo <jpjoao1001@gmail.com>
2021-04-13 19:53:48 -0300

    feat(post): added action buttons in the post footer

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,5 +1,8 @@
 import { Avatar } from '@chakra-ui/avatar';
-import { Box, Heading } from '@chakra-ui/layout';
+import { Button, ButtonGroup, IconButton } from '@chakra-ui/button';
+import { ChatIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { Box, Divider, Heading } from '@chakra-ui/layout';
+import { chakra } from '@chakra-ui/system';
 import PostDialog from 'dialogs/PostDialog';
 import { useState } from 'react';
 
@@ -14,31 +17,43 @@ const Post = ({ post, onEdit, onDelete }: PostComponentProps) => {
 
   return (
     <>
-      <Box
-        key={post.id}
-        border="1px solid #E2E8F0"
-        p={5}
-        borderRadius={5}
-        my={2}
-        onClick={() => setOpenPostDialog(true)}
-        cursor="pointer"
-      >
-        <div
-          className="ownerInfo"
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'row',
-          }}
+      <Box key={post.id} border="1px solid #E2E8F0" borderRadius={5} my={2}>
+        <chakra.div
+          cursor="pointer"
+          p={5}
+          pb="20px"
+          onClick={() => setOpenPostDialog(true)}
+          _hover={{ backgroundColor: '#f0f0f0' }}
+          transition="background-color 0.3s ease"
         >
-          <Avatar size="sm" src={post?.owner_photo_url} />
-          <Heading size="4x1" ml={3}>
-            {post?.owner_name}
-          </Heading>
-        </div>
-        <div className="postContent" style={{ marginTop: 20 }}>
-          <p>{post?.post_content}</p>
-        </div>
+          <chakra.div
+            className="ownerInfo"
+            d="flex"
+            alignItems="center"
+            flexDir="row"
+          >
+            <Avatar size="sm" src={post?.owner_photo_url} />
+            <Heading size="4x1" ml={3}>
+              {post?.owner_name}
+            </Heading>
+          </chakra.div>
+          <chakra.div mt="20px" className="postContent">
+            <p>{post?.post_content}</p>
+          </chakra.div>
+        </chakra.div>
+        <Divider />
+        <chakra.div p={2} className="postFooter">
+          <ButtonGroup color="brand" position="relative">
+            <Button variant="outline" leftIcon={<TriangleUpIcon />}>
+              99
+            </Button>
+            <IconButton
+              variant="ghost"
+              icon={<ChatIcon />}
+              aria-label="Comentários"
+            />
+          </ButtonGroup>
+        </chakra.div>
       </Box>
 
       <PostDialog
